ccbb83b61b27e6890916b4f80b6ffbc205ed640b
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2023-09-09 22:21:00 +0700

    feat: landingpage.html dan routing, style: main.html css bg-color

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def show_landing_page(request):
+    return render(request, "landingpage.html")
+
 def show_main(request):
     context = {
         'data' : [
