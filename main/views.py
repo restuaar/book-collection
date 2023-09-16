cde2c7c17add64800ca1b8b70b6ac9239c67c14a
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2023-09-16 17:06:08 +0700

    feat: dapat menampilkan data dengan json dan xml

--- main/views.py
+++ main/views.py
@@ -6,6 +6,9 @@ from django.urls import reverse
 
 from main.models import Item
 
+from django.http import HttpResponse
+from django.core import serializers
+
 # Create your views here.
 
 default_book = [
@@ -50,3 +53,20 @@ def create_book(request):
 
     # context = {'form': form}
     return render(request, "tambah_buku.html")
+
+# Mengembalikan data
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
